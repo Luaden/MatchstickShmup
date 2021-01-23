036141129ed36ccceb4e8340cab3b156ebdf3b8a
Ace Derrick Young <luadengames@gmail.com>
2021-01-23 12:34:26 +0100

    Some Basic Interfaces Added

---
+++ Assets/Scripts/Interfaces/IConfigManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IConfigManager : IAudioController, ICameraController
+{
+    AudioController AudioController { get; }
+    CameraController CameraController { get; }
+}
